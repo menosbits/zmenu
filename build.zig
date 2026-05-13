e8defdc8c9dc87696449bb48a0e6b7ce1d80caa8
Frederico Winter <frederico@diaswinter.com.br>
2026-05-13 17:25:34 -0300

    added build.zig and build.zig.zon and fixed .gitignore

---
+++ build.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe });
+
+    const apps = b.addModule("applications", .{
+        .root_source_file = b.path("src/applications.zig"),
+        .target = target,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "znur",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "applications", .module = apps },
+            },
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    run_step.dependOn(&run_cmd.step);
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const app_tests = b.addTest(.{
+        .root_module = apps,
+    });
+    const run_app_tests = b.addRunArtifact(app_tests);
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_app_tests.step);
+    test_step.dependOn(&run_exe_tests.step);
+}
