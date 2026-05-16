7d789b666d46830726ae38a31707697f9a77ca8e
Frederico Winter <frederico@diaswinter.com.br>
2026-05-15 21:59:07 -0300

    changed functions to return a list of Application structure with full path to app file

--- src/applications.zig
+++ src/applications.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
-const OpenError = std.Io.Dir.OpenError;
 
-const location = struct {
+const Location = struct {
     path: []const u8,
     home_dir: bool,
 
@@ -10,41 +9,44 @@ const location = struct {
     }
 };
 
-const locations = [6]location{
-    .{ .path = "/usr/share/applications", .home_dir = false },
-    .{ .path = "/usr/local/share/applications", .home_dir = false },
-    .{ .path = "/var/lib/snapd/desktop/applications", .home_dir = false },
-    .{ .path = "/var/lib/flatpak/exports/share/applications", .home_dir = false },
-    .{ .path = "/.local/share/applications", .home_dir = true },
-    .{ .path = "/.local/share/applications/wine/Programs", .home_dir = true },
+var locations = [6]Location{
+    .{ .path = "/usr/share/applications/", .home_dir = false },
+    .{ .path = "/usr/local/share/applications/", .home_dir = false },
+    .{ .path = "/var/lib/snapd/desktop/applications/", .home_dir = false },
+    .{ .path = "/var/lib/flatpak/exports/share/applications/", .home_dir = false },
+    .{ .path = "/.local/share/applications/", .home_dir = true },
+    .{ .path = "/.local/share/applications/wine/Programs/", .home_dir = true },
 };
 
-const app = struct {
+pub const Application = struct {
     path: []const u8,
-    description: []const u8,
-    command: []const u8,
-    terminal: bool,
+    description: []const u8 = "",
+    command: []const u8 = "",
+    terminal: bool = false,
 
-    fn new(path: []const u8) app {
-        return &app{ .path = path };
+    fn new(path: []const u8) Application {
+        return Application{ .path = path };
     }
 };
 
 // list every file in path and returns a slice of apps structs
-fn list_app_files(allocator: std.mem.Allocator, io: std.Io, path: []const u8) OpenError![]app {
-    var app_list: std.ArrayList(app) = undefined;
-    var dir = try std.Io.Dir.cwd().openDir(
+fn list_app_files(allocator: std.mem.Allocator, io: std.Io, location: Location) ![]Application {
+    var app_list = try std.ArrayList(Application).initCapacity(allocator, 1);
+    errdefer app_list.deinit(allocator);
+
+    var dir = std.Io.Dir.cwd().openDir(
         io,
-        path,
+        location.path,
         .{ .iterate = true },
-    );
+    ) catch return &[_]Application{};
     defer dir.close(io);
 
     var iter = dir.iterate();
     while (try iter.next(io)) |file| {
         const ext = std.Io.Dir.path.extension(file.name);
         if (std.mem.eql(u8, ext, ".desktop")) {
-            try app_list.append(allocator, app.new(path));
+            const new_path = try std.mem.concat(allocator, u8, &[_][]const u8{ location.path, file.name });
+            try app_list.append(allocator, Application.new(new_path));
         }
     }
 
@@ -52,19 +54,25 @@ fn list_app_files(allocator: std.mem.Allocator, io: std.Io, path: []const u8) Op
 }
 
 // list every app in system and parse their files
-pub fn find(allocator: std.mem.Allocator, io: std.Io, env_vars: *std.process.Environ.Map) !std.ArrayList(app) {
-    var app_list: std.ArrayList(app) = undefined;
-    defer app_list.deinit(allocator);
+pub fn find(allocator: std.mem.Allocator, io: std.Io, env_vars: *std.process.Environ.Map) ![]Application {
+    var app_list = try std.ArrayList(Application).initCapacity(allocator, 1);
+    errdefer app_list.deinit(allocator);
 
     // list app files
-    for (locations) |loc| {
-        if (loc.at_home()) {
+    var new_location: ?Location = null;
+    for (locations) |location| {
+        if (location.at_home()) {
             const home_dir = env_vars.get("HOME");
             var path: []const u8 = undefined;
-            path = try std.Io.Dir.path.join(allocator, &[_][]const u8{ home_dir.?, loc.path });
-            loc.path = path;
+            path = try std.Io.Dir.path.join(allocator, &[_][]const u8{ home_dir.?, location.path });
+            new_location = Location{ .path = path, .home_dir = true };
+        } else {
+            new_location = location;
         }
-        try app_list.appendSlice(allocator, list_app_files(allocator, io, loc));
+        if (new_location) |l| {
+            const listed_apps = try list_app_files(allocator, io, l);
+            app_list.appendSlice(allocator, listed_apps) catch continue;
+        }
     }
     // parse app file
 
